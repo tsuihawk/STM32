20cb23353fb8f6d24f39109a6e09234035a5df52
tsuihawk <161000279+tsuihawk@users.noreply.github.com>
2025-06-19 22:28:02 +0800

    Add files via upload
    基于systick的微秒，毫秒级别延时。

---
+++ delay.c
@@ -0,0 +1,30 @@
+// us 延时
+void delay_us(int us)
+{
+	uint32_t told = SysTick->VAL;
+	uint32_t tnow;
+	uint32_t load = SysTick->LOAD;
+	/*LOAD+1个时钟对应1ms
+	*n us对应n*(load+1)/1000
+	*/
+	uint32_t ticks = us*(load+1)/1000;
+	uint32_t cnt = 0;
+	while(1){
+		tnow =  SysTick->VAL;
+		if(told >= tnow){
+			cnt += told - tnow;
+		}else{
+			cnt += told + load + 1 - tnow;
+		}
+		if(cnt >= ticks){
+			break;
+		}
+	}
+}
+// ms延时
+void delay_ms(int ms)
+{
+	for(int i = 0;i < ms;i++){
+		delay_us(1000);
+	}
+}
